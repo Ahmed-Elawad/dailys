e02e8538828845db8b94d9747494faa56dba49ec
Ahmed-Elawad <a.elawad97@gmail.com>
2020-12-16 14:01:43 -0500

    Pt1

---
+++ java/account.java
@@ -0,0 +1,7 @@
+// import all utility modules from java
+import java.util.*;
+
+// define an account class to be extended by a checking, savings, safe, and password validator
+public class Account {
+    
+}
