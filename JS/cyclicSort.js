7d4d1c1224cc013432d3ef2efe60755e92067199
Ahmed-Elawad <a.elawad97@gmail.com>
2021-01-06 11:07:18 -0500

    cyclicSort complete

---
+++ JS/cyclicSort.js
@@ -0,0 +1,45 @@
+/**
+ * We are given an array containing ‘n’ objects. Each object, when created,
+ * was assigned a unique number from 1 to ‘n’ based on their creation sequence.
+ * This means that the object with sequence number ‘3’ was created just 
+ * before the object with sequence number ‘4’.
+ *
+ * Write a function to sort the objects in-place on their creation sequence
+ * number in O(n) and without any extra space. For simplicity, let’s 
+ * assume we are passed an integer array containing only the sequence numbers,
+ * though each number is actually an object.
+ *
+ *Example 1:
+ *
+ *Input: [3, 1, 5, 4, 2]
+ *Output: [1, 2, 3, 4, 5]
+ *Example 2:
+ *
+ *Input: [2, 6, 4, 3, 1, 5]
+ *Output: [1, 2, 3, 4, 5, 6]
+ *Example 3:
+ *
+ *Input: [1, 5, 6, 4, 3, 2]
+ *Output: [1, 2, 3, 4, 5, 6]
+*/
+
+const sort = (numArr) => {
+    
+    let numOfChanges = 0;
+    for (let i = 0; i < numArr.length; i++) {
+        let currentVal = numArr[i];
+        while (currentVal - 1 !== i) {
+            let temp = numArr[currentVal -  1];
+            [numArr[i], numArr[currentVal - 1]] = [temp, currentVal];
+            currentVal = numArr[i];
+            numOfChanges++;
+        }
+        if (numOfChanges === numArr.length) break;
+    }
+
+    return numArr;
+};
+
+console.log(sort([3, 1, 5, 4, 2])); // => [1,2,3,4,5]
+console.log(sort([2, 6, 4, 3, 1, 5])); // => [1, 2, 3, 4, 5, 6]
+console.log(sort([1, 5, 6, 4, 3, 2])); // => [1, 2, 3, 4, 5, 6]
